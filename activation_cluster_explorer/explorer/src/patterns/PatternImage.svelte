c335a86844d4949f784966c2ec0968d644743dcf
Alex <alex.baeuerle@uni-ulm.de>
2022-03-03 12:28:04 +0100

    Icon for misclassified samples

--- activation_cluster_explorer/explorer/src/patterns/PatternImage.svelte
+++ activation_cluster_explorer/explorer/src/patterns/PatternImage.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   import { tooltip } from "../stores";
+  import Fa from "svelte-fa";
+  import FaLayers from "svelte-fa/src/fa-layers.svelte";
+  import { faPlay } from "@fortawesome/free-solid-svg-icons/faPlay";
+  import { faExclamation } from "@fortawesome/free-solid-svg-icons/faExclamation";
   import type { PatternForSample } from "../types";
 
   export let imagePath: string;
@@ -25,21 +29,31 @@
   }
 </script>
 
-<img
-  class="h-32"
-  src={imagePath}
-  on:mouseenter={() => {
-    if (sample !== undefined && layer !== undefined) {
-      hover = true;
-      updateSampleTooltip();
-    }
-  }}
-  on:mouseleave={() => {
-    if (sample !== undefined && layer !== undefined) {
-      hover = false;
-      updateSampleTooltip();
-    }
-  }}
-  on:mousemove={handleMousemove}
-  alt="Data Sample"
-/>
+<div class="relative">
+  <img
+    class="h-32"
+    src={imagePath}
+    on:mouseenter={() => {
+      if (sample !== undefined && layer !== undefined) {
+        hover = true;
+        updateSampleTooltip();
+      }
+    }}
+    on:mouseleave={() => {
+      if (sample !== undefined && layer !== undefined) {
+        hover = false;
+        updateSampleTooltip();
+      }
+    }}
+    on:mousemove={handleMousemove}
+    alt="Data Sample"
+  />
+  {#if sample !== undefined && sample.label !== undefined && sample.prediction !== undefined && sample.label !== sample.prediction}
+    <div class="absolute top-1 right-2">
+      <FaLayers>
+        <Fa icon={faPlay} rotate={-90} color="#ff453a" />
+        <Fa icon={faExclamation} scale={0.5} translateY={0.1} color="white" />
+      </FaLayers>
+    </div>
+  {/if}
+</div>
