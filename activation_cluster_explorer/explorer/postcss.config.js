6ab1fe3507a89d257782f67bbc4c467b4fd6db1a
Alex <alex.baeuerle@uni-ulm.de>
2022-03-17 10:03:02 +0100

    add ci for frontend

--- activation_cluster_explorer/explorer/postcss.config.js
+++ activation_cluster_explorer/explorer/postcss.config.js
@@ -2,16 +2,6 @@ const purgecss = require('@fullhuman/postcss-purgecss');
 const purgeSvelte = require('purgecss-from-svelte');
 const env = process.env.NODE_ENV;
 
-// Custom PurgeCSS extractor for Tailwind that allows special characters in
-// class names.
-//
-// https://github.com/FullHuman/purgecss#extractor
-class TailwindExtractor {
-  static extract(content) {
-    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
-  }
-}
-
 module.exports = {
   plugins: [
     require('postcss-import')(),
@@ -31,13 +21,6 @@ module.exports = {
             {
               extractor: purgeSvelte,
               extensions: ['svelte'],
-            },
-            {
-              extractor: TailwindExtractor,
-
-              // Specify the file extensions to include when scanning for
-              // class names.
-              extensions: ['html'],
             },
           ],
         }),
