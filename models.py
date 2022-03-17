d7a2f6d019d05d2d850ae603f9ff472e769ebada
Alex <alex.baeuerle@uni-ulm.de>
2022-03-17 08:55:27 +0100

    update pipeline and variable for model data

--- models.py
+++ models.py
@@ -40,37 +40,37 @@ def get_data_set(data_path, data_set, data_set_size, split='test'):
             file_names, image_dir)
 
 
-def setup_mnist(data_pipeline):
+def setup_mnist(data_set):
 
     def normalize_img(image):
         """Normalizes images: `uint8` -> `float32`."""
         return tf.cast(image, tf.float32) / 255.
-    data_pipeline = data_pipeline.map(normalize_img,
-                                      num_parallel_calls=tf.data.AUTOTUNE)
+    data_set = data_set.map(normalize_img,
+                            num_parallel_calls=tf.data.AUTOTUNE)
 
     model_save_name = 'mnist_classifier'
     path = F"{model_save_name}"
     model = tf.keras.models.load_model(path)
-    return model, data_pipeline
+    return model, data_set
 
 
-def setup_cifar10(data_pipeline):
+def setup_cifar10(data_set):
 
     def normalize_img(image):
         """Normalizes images: `uint8` -> `float32`."""
         return tf.cast(image, tf.float32) / 255.
 
-    data_pipeline = data_pipeline.map(normalize_img,
-                                      num_parallel_calls=tf.data.AUTOTUNE)
+    data_set = data_set.map(normalize_img,
+                            num_parallel_calls=tf.data.AUTOTUNE)
 
     model_save_name = 'cifar10_classifier'
     path = F"{model_save_name}"
     model = tf.keras.models.load_model(path)
     model.summary()
-    return model, data_pipeline
+    return model, data_set
 
 
-def setup_inception_v1(data_pipeline):
+def setup_inception_v1(data_set):
     model = inception_v1()
 
     def transform_images(image, new_size):
@@ -78,19 +78,19 @@ def setup_inception_v1(data_pipeline):
         img = tf.image.convert_image_dtype(img, tf.float32)
         img = tf.image.resize(img, [new_size, new_size])
         return img
-    data_pipeline = data_pipeline.map(lambda row: transform_images(row, 224),
-                                      num_parallel_calls=tf.data.AUTOTUNE)
-    return model, data_pipeline
+    data_set = data_set.map(lambda row: transform_images(row, 224),
+                            num_parallel_calls=tf.data.AUTOTUNE)
+    return model, data_set
 
 
-def setup_inception_v3(data_pipeline):
+def setup_inception_v3(data_set):
     def transform_images(image, new_size):
         return tf.keras.applications.inception_v3.preprocess_input(
             tf.keras.layers.Resizing(new_size, new_size)(image))
-    data_pipeline = data_pipeline.map(lambda row: transform_images(row, 299),
-                                      num_parallel_calls=tf.data.AUTOTUNE)
+    data_set = data_set.map(lambda row: transform_images(row, 299),
+                            num_parallel_calls=tf.data.AUTOTUNE)
 
     model = tf.keras.applications.InceptionV3(
         include_top=True, weights='imagenet')
 
-    return model, data_pipeline
+    return model, data_set
