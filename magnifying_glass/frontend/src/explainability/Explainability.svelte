aced1d4433f53b391872748084a07b1ffdfd33dd
Alex <alex.baeuerle@uni-ulm.de>
2022-07-08 08:50:44 +0100

    add toggle for feature vis

--- magnifying_glass/frontend/src/explainability/Explainability.svelte
+++ magnifying_glass/frontend/src/explainability/Explainability.svelte
@@ -2,19 +2,17 @@
   import MaxActivations from "./MaxActivations.svelte";
   import FeatureVis from "./FeatureVis.svelte";
 
-  import { fetchFeatureVisExists } from "../api";
-  import { layer, model } from "../stores";
+  import { patternsWidth } from "../stores";
 
   export let maxActivating: string[];
-
-  $: featureVisRequest = fetchFeatureVisExists($model, $layer);
+  export let featureVis: boolean;
 </script>
 
-<div class="flex flex-1 flex-col min-h-0 h-full">
-  {#await featureVisRequest then}
+<div class="flex flex-col min-h-0 h-full" style={`width: ${$patternsWidth}px;`}>
+  {#if maxActivating.length > 0}
     <MaxActivations {maxActivating} />
+  {/if}
+  {#if featureVis}
     <FeatureVis />
-  {:catch}
-    <MaxActivations {maxActivating} />
-  {/await}
+  {/if}
 </div>
